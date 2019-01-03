bfbb6f2697165472412fe7e887fa436c5d7daae5
Nathan Coburn <nathanlcoburn@gmail.com>
2019-01-03 00:28:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
 /* Enter the code to remove the main node element under this comment */
-
+let ul = document.getElementById('main');
+ul.remove('main');
 /* Create your new element here and assign it to newHeader */
-const newHeader = null;
+/*const newHeader = victory;
+
+var element = document.createElement(victory['h1']);
+let h1 = "Nathan is the champion!"; */
+
+let newHeader = document.createElement('h1');
+
+
+function addElement (newHeader) { 
+  // create a new div element 
+  var newContent = document.createTextNode("Nathan is the champion!"); 
+  // add the text node to the newly created div
+  newHeader.appendChild(newContent);
+}
+
+newHeader.id ('victory');
+  var newContent = document.createTextNode("Nathan is the champion!");
